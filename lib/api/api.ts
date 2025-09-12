d16aa348f32b89401f41f71029ac8988b505412b
Андрій <gapeev1979@gmail.com>
2025-09-12 20:15:06 +0300

    fix lib/api

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 export const nextServer = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL + '/api',
+  baseURL: '/api',
+
+  // baseURL: process.env.NEXT_PUBLIC_API_URL + '/api',
   withCredentials: true,
 });
 //  (const baseURL = process.env.NEXT_PUBLIC_API_URL + '/api';)
